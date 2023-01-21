579426bde10479312ca0c26ed7f1577fa27b1f77
Oxy26 <oxygenium.26@gmail.com>
2023-01-21 21:10:37 +0200

    Final Changes

--- JS2script.js
+++ JS2script.js
@@ -1,23 +1,40 @@
 function sayIt() {
-console.log ( "Я учу Java Script" );
+    console.log ( "Я учу Java Script" );
 }
 sayIt ();
 
 let id = 1;
 
-function forward () {
+function forward() {
     document.getElementById("picture" + id).style.display = "none";
 
     if (id == 1) {
         id = 2;
     }
     else if (id == 2) {
+        id = 3;
+    } 
+    else if (id == 3) {
         id = 1;
     } 
+
+    console.log(document.getElementsByTagName("img").length);
 
     document.getElementById("picture" + id).style.display = "block";
 }
 
-function previous () {
- id = 1;
+function backward() {
+    document.getElementById("picture" + id).style.display = "none";
+
+    if (id == 1) {
+        id = 3;
+    }
+    else if (id == 2) {
+        id = 1;
+    } 
+    else if (id == 3) {
+        id = 2;
+    } 
+
+    document.getElementById("picture" + id).style.display = "block";    
 }
